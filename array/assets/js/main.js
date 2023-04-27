fcfa92f32fd2657d2affb910335770fd92b0b049
Konul Yusifova <127186343+konulyusifova@users.noreply.github.com>
2023-04-27 08:51:04 +0400

    Add files via upload

---
+++ array/assets/js/main.js
@@ -0,0 +1,19 @@
+// 1 herf axtarmasi:
+
+// var text = prompt("metn daxil edin:");
+// var arr = prompt("deyer daxil edin:");
+// var count = 0;
+// for (let i = 0; i < text.length; i++) {
+//     if (arr == text[i]) {
+//         count++;
+//     }
+// }
+// console.log(count);
+
+
+
+// String search metodu:(match da nece herf yazsaq onu axtarir)
+
+// var text=prompt("Mətn daxil edin:");
+// var mytext=text.match(/kon/gi);
+// console.log(mytext);
